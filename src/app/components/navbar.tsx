627ebf181f7e89ea9f0b728ebb76233546826115
esdras-kyo <esdrascamel@gmail.com>
2025-09-04 21:29:15 -0300

    improve navbar with motion and mobile support

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { MenuIcon, X } from "lucide-react";
 import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 /*
 ministerios
 historia
@@ -13,54 +14,59 @@ contato
 */
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const links = [
+    { href: "/", label: "NOSSA HISTORIA" },
+    { href: "/", label: "MINISTÉRIOS" },
+    { href: "/", label: "PROGRAMAÇÃO" },
+    { href: "/", label: "CONTATO" },
+    { href: "/", label: "CONTRIBUA" },
+    { href: "/", label: "EVENTOS" },
+  ];
   return (
     <div className="fixed top-0 left-0 w-full z-5 shadow-md">
-      <div className="bg-black/40 md:bg-transparent  max-w-7xl mx-auto flex items-center justify-between p-6">
+      <div className=" md:bg-transparent  max-w-7xl mx-auto flex items-center justify-between p-6">
         <div className="text-xl font-bold">
           <Link href="/" className="hover:text-gray-300">
             Logo
           </Link>
         </div>
-        <div className=" flex md:hidden " onClick={()=>setIsOpen(!isOpen)}>
-         {!isOpen ?<MenuIcon />: <X/>}
+        <div className=" flex md:hidden " onClick={() => setIsOpen(!isOpen)}>
+          {!isOpen ? <MenuIcon /> : <X />}
         </div>
-        <nav
-          className={`flex ${
-            !isOpen ? "hidden md:flex" : "flex"
-          } space-x-8 space-y-8 flex-col md:flex-row`}
-        >
-          <div className="text-xs font-mono ">
-            <Link href="/" className="hover:text-gray-300">
-              NOSSA HISTORIA
+
+        <div className="hidden md:flex space-x-8 text-xs font-mono">
+          {links.map((link) => (
+            <Link key={link.label} href={link.href} className="hover:text-gray-300">
+              {link.label}
             </Link>
-          </div>
-          <div className="text-xs font-mono ">
-            <Link href="/" className="hover:text-gray-300">
-              MINISTÉRIOS
+          ))}
+        </div>
+        </div>
+      <AnimatePresence initial={false}
+      mode="wait">
+        {isOpen && (
+          <motion.div
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            exit={{ y: -50, opacity: 0 }}
+            className="flex flex-col items-center  justify-center  space-y-6  md:hidden bg-black/50 p-4"
+          >
+          {links.map((link) => (
+            <Link
+              key={link.label}
+              href={link.href}
+              onClick={()=>setIsOpen(!isOpen)}
+              className="text-xs text-center font-mono hover:text-gray-300"
+            >
+              {link.label}
             </Link>
-          </div>
-          <div className="text-xs font-mono ">
-            <Link href="/" className="hover:text-gray-300">
-              PROGRAMAÇÃO
-            </Link>
-          </div>
-          <div className="text-xs font-mono ">
-            <Link href="/" className="hover:text-gray-300">
-              CONTATO
-            </Link>
-          </div>
-          <div className="text-xs font-mono ">
-            <Link href="/" className="hover:text-gray-300">
-              CONTRIBUA
-            </Link>
-          </div>
-          <div className="text-xs font-mono ">
-            <Link href="/" className="hover:text-gray-300">
-              EVENTOS
-            </Link>
-          </div>
-        </nav>
-      </div>
+          ))}
+          </motion.div>
+          
+        )}
+        </AnimatePresence>
+      
     </div>
   );
 }
