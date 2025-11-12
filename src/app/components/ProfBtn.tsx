7e8ff307afc61c19b9b4eb6574db2eea5e6e039e
esdras-kyo <esdrascamel@gmail.com>
2025-11-12 12:10:03 -0300

    verify role on profile button

--- src/app/components/ProfBtn.tsx
+++ src/app/components/ProfBtn.tsx
@@ -1,32 +1,35 @@
-"use client"
-import { User2 } from "lucide-react"
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+"use client";
+import { User2 } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { useUserClaims } from "../hooks/useUserClaims";
 
+export default function ProfBtn() {
+  const { claims } = useUserClaims();
+  const isLeader = claims?.roles?.some((r) => r.role === "LEADER");
+  // const isMember = claims?.roles?.some((r) => r.role === "MEMBER");
+  // const isVisitant = claims?.roles?.some((r) => r.role === "VISITANT");
+  const isAdmin = claims?.roles?.some((r) => r.role === "ADMIN");
 
-export default function ProfBtn(){
-    // const route = useRouter()
-    // const [isOpen, setIsOpen] = useState(false)
-
-
-    return(
-        <div className="">
-   
-            <SignedIn>
-            <UserButton
-     userProfileUrl="/conta" // possivel rota de perfil
-
-    >
-      <UserButton.MenuItems>
-        {/* Link direto pro painel de líder */}
-        <UserButton.Link label="Painel do Líder" href="/leader" labelIcon={<User2 className="w-4"/>} />
-
-      </UserButton.MenuItems>
-    </UserButton>
+  return (
+    <div className="">
+      <SignedIn>
+        <UserButton
+          userProfileUrl="/conta" // possivel rota de perfil
+        >
+          <UserButton.MenuItems>
+            {isLeader || isAdmin ? (
+              <UserButton.Link
+                label="Painel do Líder"
+                href="/leader"
+                labelIcon={<User2 className="w-4" />}
+              />
+            ) : null}
+          </UserButton.MenuItems>
+        </UserButton>
       </SignedIn>
       <SignedOut>
         <SignInButton />
       </SignedOut>
-
-        </div>
-    )
+    </div>
+  );
 }
