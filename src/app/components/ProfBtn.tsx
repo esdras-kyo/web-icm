4a8c2620e233758a86ba7ecada6ac6fb9964743e
esdras-kyo <esdrascamel@gmail.com>
2025-12-15 12:07:37 -0300

    fix clerk error

--- src/app/components/ProfBtn.tsx
+++ src/app/components/ProfBtn.tsx
@@ -1,75 +1,72 @@
 "use client";
-import { FlaskConical, ShieldUser, User, User2 } from "lucide-react";
+
+import { FlaskConical, ShieldUser, User2 } from "lucide-react";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { useUserClaims } from "../hooks/useUserClaims";
 
 export default function ProfBtn() {
   const { claims } = useUserClaims();
   const isLeader = claims?.roles?.some((r) => r.role === "LEADER");
-  // const isMember = claims?.roles?.some((r) => r.role === "MEMBER");
-  // const isVisitant = claims?.roles?.some((r) => r.role === "VISITANT");
   const isAdmin = claims?.roles?.some((r) => r.role === "ADMIN");
 
   return (
-    <div className="">
+    <div>
       <SignedIn>
-        <UserButton
-          userProfileUrl="/conta" // possivel rota de perfil
-        >
+        <UserButton userProfileUrl="/conta">
           <UserButton.MenuItems>
-            {isLeader || isAdmin ? (
+            {(isLeader || isAdmin) && (
               <UserButton.Link
                 label="Painel do Líder"
                 href="/leader"
-                labelIcon={<User2 className="w-4" />}
+                labelIcon={<User2 className="w-4 h-4" />}
               />
-            ) : null}
-            {isAdmin ? ( 
+            )}
+
+            {isAdmin && (
               <UserButton.Link
                 label="Painel Administrativo"
                 href="/offc"
-                labelIcon={
-                  <div className="flex flex-col items-start justify-start">
-                    <ShieldUser className="w-5" />
-                  </div>
-                } />) : null} 
+                labelIcon={<ShieldUser className="w-4 h-4" />}
+              />
+            )}
 
-            {isAdmin ? (
-
+            {isAdmin && (
               <UserButton.Link
                 label="Historia - teste"
                 href="/historia"
-                labelIcon={<FlaskConical className="w-4" />}
+                labelIcon={<FlaskConical className="w-4 h-4" />}
               />
-            ) : null} {isAdmin ? (
-              
+            )}
+
+            {isAdmin && (
               <UserButton.Link
                 label="Agenda - teste"
                 href="/agenda"
-                labelIcon={<FlaskConical className="w-4" />}
+                labelIcon={<FlaskConical className="w-4 h-4" />}
               />
-            ) : null} {isAdmin ? (
+            )}
 
+            {isAdmin && (
               <UserButton.Link
                 label="Celulas - teste"
                 href="/celulas"
-                labelIcon={<FlaskConical className="w-4" />}
+                labelIcon={<FlaskConical className="w-4 h-4" />}
               />
-              ) : null}
+            )}
 
             <UserButton.Link
               label="Área do Membro"
               href="/conta/personal"
-              labelIcon={<User2 className="w-4" />}
+              labelIcon={<User2 className="w-4 h-4" />}
             />
-
           </UserButton.MenuItems>
         </UserButton>
       </SignedIn>
+
       <SignedOut>
         <SignInButton>
-          <button className="flex cursor-pointer rounded-full border w-7 h-7 justify-center">
-            <User2 className="w-4 text-white" />
+          <button className="flex cursor-pointer rounded-full border w-7 h-7 justify-center items-center">
+            <User2 className="w-4 h-4 text-white" />
           </button>
         </SignInButton>
       </SignedOut>
